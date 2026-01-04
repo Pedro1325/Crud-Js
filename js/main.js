77852e7a0fe911393537ec872687ab1b40d9bb46
Pedro1325 <policiamurder@gmail.com>
2026-01-04 15:46:36 -0300

    Adicionar projeto CRUD completo

---
+++ js/main.js
@@ -0,0 +1,40 @@
+import ui from "./ui.js"
+import api from "./api.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+  ui.renderizarPensamentos()
+
+  const formularioPensamento = document.getElementById("pensamento-form")
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+})
+
+
+try {
+  if(id) {
+    await api.editarPensamento({ id, conteudo, autoria})
+  } else {
+    await api.salvarPensamento({ conteudo, autoria })
+
+  }
+  ui.renderizarPensamentos()
+}
+catch {
+  alert("Erro ao salvar Pensamento")
+}
+
+
+async function manipularSubmissaoFormulario(event) {
+  event.preventDefault();
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
+
+  try {
+    await api.salvarPensamento({conteudo, autoria})
+    ui.renderizarPensamentos()
+  }
+  catch {
+          alert("Erro ao Salvar Pensamento, tente novamente ou mais tarde")
+  }
+
+}
